3c66e158f5975ca01882c79f2524d8495b8dc071
FredrikAndreasson <fredrik.andreasson@yandex.com>
2017-09-19 08:12:12 +0200

    My changes 2/2
    adding my changes to master because my branch of github is fucked upj,

---
+++ AnimalSlaughter/AnimalSlaughter/PointSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace AnimalSlaughter
+{
+    class PointSystem
+    {
+        public bool myYouHaveWon = false;
+        int myPoints;
+        int myPointsToWin;
+        public PointSystem(int somePointsToWin)
+        {
+            myPointsToWin = somePointsToWin;
+        }
+        public void UpdateScore(int somePointsToAdd)
+        {
+            AddScore(somePointsToAdd);
+            WinCheck(myPoints);
+        }
+        private void AddScore(int aScoreToAdd)
+        {
+            myPoints += aScoreToAdd;
+        }
+        private void WinCheck(int someScore)
+        {
+            if (someScore > myPointsToWin)
+            {
+                myYouHaveWon = true;
+                //You win somehow (I haven't figured it out yet)
+            }
+        }
+    }
+}
